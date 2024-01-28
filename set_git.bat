7e14cd0fa0ce1c7f1a02a3b6eb5544f63587a1ae
Haya Kabishi <you@example.com>
2024-01-28 18:55:21 +0200

    Sun 01-28-24

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/HayaKabishi531/lecture-2.git
+git remote add origin https://github.com/HayaKabishi531/LectureWEB1.git
 git push -u origin main
