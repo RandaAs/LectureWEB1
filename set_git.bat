4e498b57bcf6f3379af9a8e7d64a5619cefe7e47
Randa Assadi <Randa24401@gmail.com>
2024-01-31 08:47:22 +0200

    new

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/RandaAs/Randa-Assadi.git
+git remote add origin https://github.com/RandaAs/Lab2Web.git
 git push -u origin main
